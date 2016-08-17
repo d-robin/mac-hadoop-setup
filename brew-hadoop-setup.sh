3f4d0d9ce20d1276ba395f303fb8cd24d8c80c4d
Robin <d.robin@gmail.com>
2016-08-17 09:52:06 -0500

    Create brew-hadoop-setup.sh

---
+++ brew-hadoop-setup.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# brew-hadoop-setup.sh, version 1.0
+# 
+# Run the script as root.
+# 
+# Info: Quickly setup dev hadoop environment using brew on mac. Contains basic 
+#       configuration for cluster mode deployment on a single node.
+# 
+# -- Robin (d.robin@gmail.com) -- 2016-08 --
+
+
+# 1. Directory for HDFS
+mkdir -p /opt/hadoop
+
+# 2. Setup core-site.xml
+corepath=$(find /usr/local/Cellar/hadoop -name core-site.xml | \grep 'etc/hadoop')
+cp $corepath $(dirname $corepath)/core-site.xml.bak_`date +%Y-%m-%d--%H-%M-%S`
+
+echo 
+'<configuration>
+    <property>
+        <name>hadoop.tmp.dir</name>
+        <value>/opt/hadoop/tmp/hadoop-${user.name}</value>
+        <description>A base for other temporary directories.</description>
+    </property>
+    <property>
+        <name>fs.defaultFS</name>
+        <value>hdfs://localhost:9000</value>
+    </property>
+</configuration>' > $corepath
+
+
+# 3. Setup hdfs-site.xml
+hdfssite=$(find /usr/local/Cellar/hadoop -name hdfs-site.xml | \grep 'etc/hadoop')
+cp $hdfssite $(dirname $hdfssite)/hdfs-site.xml.bak_`date +%Y-%m-%d--%H-%M-%S`
+
+echo 
+'<configuration>
+    <property>
+        <name>dfs.replication</name>
+        <value>1</value>
+    </property>
+    <property>
+        <name>dfs.namenode.name.dir</name>
+        <value>/opt/hadoop/data/dfs.namenode.name.dir</value>
+    </property>
+    <property>
+        <name>dfs.datanode.data.dir</name>
+        <value>/opt/hadoop/data/dfs.datanode.data.dir</value>
+    </property>
+</configuration>' > $hdfssite
+
+
+# 4. Setup yarn-site.xml
+yarnsite=$(find /usr/local/Cellar/hadoop -name yarn-site.xml | \grep 'etc/hadoop')
+cp $yarnsite $(dirname $yarnsite)/yarn-site.xml.bak_`date +%Y-%m-%d--%H-%M-%S`
+
+echo 
+'<configuration>
+
+<!-- Site specific YARN configuration properties -->
+  <property>
+    <name>yarn.nodemanager.aux-services</name>
+    <value>mapreduce_shuffle</value>
+  </property>
+</configuration>' > $yarnsite
+
+
+# 5. Setup mapred-site.xml
+# Do not run the following commands if you are not using Apache Tez
+mapredsite=$(find /usr/local/Cellar/hadoop -name mapred-site.xml | \grep 'etc/hadoop')
+cp $mapredsite $(dirname $mapredsite)/mapred-site.xml.bak_`date +%Y-%m-%d--%H-%M-%S`
+
+echo 
+'<configuration>
+    <property>
+        <name>mapreduce.framework.name</name>
+        <value>yarn-tez</value>
+    </property>
+</configuration>' > $mapredsite
+
+# End Tez configuration
+
+# 6. enable start-dfs.sh & start.yarn.sh
+echo '
+
+Add following export to .bash_profile to use commands start-dfs.sh & start-yarn.sh,
+
+export PATH="/usr/local/sbin:$PATH"
+
+'
